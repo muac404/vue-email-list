df0e6302a3c58468d82acefbec2340f283b55eef
muac404 <muac404@gmail.com>
2022-11-16 16:54:03 +0100

    Aggiunge: Stile, Logo Boolean, generatore random js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,20 @@
   createApp({
     data() {
       return {
-        message: 'Hello Vue!'
+        emails: []
       }
+    },
+    methods: {
+
+    },
+    created(){
+        for(let i = 0; i <= 10; i++){
+            axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+            .then((response) => {
+                this.emails.push(response.data.response);
+                
+            });
+        }
+        
     }
   }).mount('#app')
