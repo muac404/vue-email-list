180d049634ca5b104b7d2a20499ff128b2b84729
muac404 <muac404@gmail.com>
2022-11-16 17:03:38 +0100

    Corregge contatore i che generava 11 indirizzi email

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@
 
     },
     created(){
-        for(let i = 0; i <= 10; i++){
+        for(let i = 1; i <= 10; i++){
             axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
             .then((response) => {
                 this.emails.push(response.data.response);
